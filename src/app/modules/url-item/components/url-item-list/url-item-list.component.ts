3b1dc7172d502f08eaa01190b86536821d45ffee
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-23 17:05:50 +0200

    url-item-list component renamed

--- src/app/modules/url-item/components/url-item-list/url-item-list.component.ts
+++ src/app/modules/url-item/components/url-item-list/url-item-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-url-item-list',
+  selector: 'url-item-list',
   templateUrl: './url-item-list.component.html',
   styleUrls: ['./url-item-list.component.scss']
 })
