56fc368aaa1d3551cb0071b1243691cb518f74ef
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-23 14:05:13 +0200

    Basic url-item-home.component created

--- src/app/modules/url-item/components/url-item-home/url-item-home.component.ts
+++ src/app/modules/url-item/components/url-item-home/url-item-home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UrlItemConnector } from '../../connectors';
+import { UrlItem } from '../../models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-url-item-home',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./url-item-home.component.scss']
 })
 export class UrlItemHomeComponent {
+
+  protected urlItems$!: Observable<UrlItem[]>;
+
+  constructor(protected urlItemConnector: UrlItemConnector) {
+  }
+
+  ngOnInit(): void {
+    this.urlItems$ = this.urlItemConnector.findAll();
+  }
 
 }
