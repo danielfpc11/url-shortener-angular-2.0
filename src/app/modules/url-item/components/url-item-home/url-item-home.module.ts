7886b04f33a2be9c2ad88c17f7560cc4909faa48
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-23 18:11:33 +0200

    simple url-item-home.component

--- src/app/modules/url-item/components/url-item-home/url-item-home.module.ts
+++ src/app/modules/url-item/components/url-item-home/url-item-home.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UrlItemHomeComponent } from './url-item-home.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { UrlItemListModule } from '../url-item-list';
 
 @NgModule({
   declarations: [
     UrlItemHomeComponent
   ],
+  exports: [
+    UrlItemHomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
+    UrlItemListModule
   ]
 })
 export class UrlItemHomeModule {
