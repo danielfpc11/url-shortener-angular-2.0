b9d144a16394169a9221fe4414f3b1a18a859534
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-23 18:11:41 +0200

    simple url-item-list.component

--- src/app/modules/url-item/components/url-item-list/url-item-list.component.ts
+++ src/app/modules/url-item/components/url-item-list/url-item-list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { UrlItem } from '../../models';
+import { Subscription, tap } from 'rxjs';
+import { UrlItemConnector } from '../../connectors';
+import { ClipboardService } from '../../../../shared';
 
 @Component({
   selector: 'url-item-list',
   templateUrl: './url-item-list.component.html',
   styleUrls: ['./url-item-list.component.scss']
 })
-export class UrlItemListComponent {
+export class UrlItemListComponent implements OnDestroy {
+
+  @Input() urlItems!: UrlItem[];
+  protected subscription: Subscription = new Subscription();
+
+  constructor(protected urlItemConnector: UrlItemConnector,
+              protected clipboardService: ClipboardService) {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  protected copyShortenedUrlToClipboard(shortenedUrlId: string): void {
+    this.subscription.add(this.clipboardService
+                              .copyToClipboard(this.getShortenedUrl(shortenedUrlId))
+                              .subscribe());
+  }
+
+  protected deleteUrlItem(id: number): void {
+    this.subscription.add(this.urlItemConnector
+                              .delete(id)
+                              .pipe(tap((): void => this.deleteUrlItemFromList(id)))
+                              .subscribe());
+  }
+
+  private getShortenedUrl(shortenedUrlId: string): string {
+    return `${location.origin}/shortened/${shortenedUrlId}`;
+  }
+
+  private deleteUrlItemFromList(id: number): void {
+    this.urlItems = this.urlItems.filter((urlItem: UrlItem): boolean => urlItem.id !== id);
+  }
 
 }
--- src/app/modules/url-item/components/url-item-list/url-item-list.module.ts
+++ src/app/modules/url-item/components/url-item-list/url-item-list.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UrlItemListComponent } from './url-item-list.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     UrlItemListComponent
   ],
+  exports: [
+    UrlItemListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ]
 })
 export class UrlItemListModule {
