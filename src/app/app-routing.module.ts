4f59a566d1887cea076681c1a3bc070df8bfb557
Daniel Pulgarin <danielfpc11@gmail.com>
2024-10-23 14:04:56 +0200

    added route to UrlItemHomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UrlItemHomeComponent } from './modules';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UrlItemHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
